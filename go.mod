3b030428c25e31b18c3548a4cfc8fa553e0ffd89
random-heap <452465634@qq.com>
2020-09-02 10:24:31 +0800

    单元测试

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module go-learning
 go 1.13
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/julienschmidt/httprouter v1.3.0
 )
---
+++ test/student_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	db := DBConnect{
+		DBUrl:"root:root1243@tcp(127.0.0.1:3306)/test?charset=utf8mb4",
+	}
+	var err error
+	db.DB, err = sql.Open("mysql", db.DBUrl)
+	if err != nil {
+		fmt.Printf("sql connect error : %s", err)
+		return
+	}
+	fmt.Println(db.DBUrl)
+	fmt.Println(db.DB.Stats())
+	db.queryStudent()
+
+}
